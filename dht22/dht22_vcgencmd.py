30e149f6a2b5c740c6d3721a8669c195d8a9615a
razielar <razielar@gmail.com>
2023-10-29 10:49:26 +0100

    remove the vcgencmd part

--- dht22/dht22_vcgencmd.py
+++ dht22/dht22_vcgencmd.py
@@ -19,7 +19,6 @@ TOKEN = os.environ['DOCKER_INFLUXDB_INIT_ADMIN_TOKEN']
 ORG = os.environ['DOCKER_INFLUXDB_INIT_ORG']
 BUCKET = os.environ['DOCKER_INFLUXDB_INIT_BUCKET']
 DELAY = os.environ["DHT_VCGENCM_INTERVAL"]
-VCGENCMD_DATA = os.environ["VCGENCMD_DATA"]
 TEMPERATURE_UNIT = os.environ["TEMPERATURE_UNIT"]
 
 def _log_influxdb(url: str, token: str, org: str, bucket: str, sensor_type: str, values: List[float]) -> None:
@@ -36,22 +35,6 @@ def _log_influxdb(url: str, token: str, org: str, bucket: str, sensor_type: str,
         p = Point("vcgencm").field("cpu_clock", values[0]).field("cpu_voltage", values[1]).field("soc_temp", values[2])
     write_api.write(bucket=bucket, org=org, record=p)
 
-def _vcgencm(volume_path: str) -> None:
-    """
-    Get and log: the vcgencm stdout from the vcgencm container, reading the last line from the tsv file.
-    Args:
-        volume_path: the path of the vcgencm container
-    """
-    with open(volume_path, "r") as tsv_file:
-        lines = tsv_file.readlines()
-        if lines:
-            last_line = lines[-1]
-            data = last_line.strip().split('\t')
-            cpu_clock = float(data[0])
-            cpu_voltage = float(data[1])
-            soc_temp = float(data[2])
-            _log_influxdb(url=URL, token=TOKEN, org=ORG, bucket=BUCKET, sensor_type="vcgencm", values=[cpu_clock, cpu_voltage, soc_temp])
-
 def main(delay: float, condition: bool=True) -> None:
     """
     Get and log: Temperature & Humidity from the dht22 sensor and vcgencm stdout.
@@ -61,7 +44,6 @@ def main(delay: float, condition: bool=True) -> None:
     dhtDevice = adafruit_dht.DHT22(board.D2, use_pulseio=False)
     while condition:
         try:
-            _vcgencm(volume_path= VCGENCMD_DATA)
             humidity = dhtDevice.humidity
             if TEMPERATURE_UNIT == "Celsius":
                 temperature = dhtDevice.temperature
